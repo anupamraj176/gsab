fd4bafafa0de96e71213d55e71a8c897e517436f
anupamraj176 <147419746+anupamraj176@users.noreply.github.com>
2025-10-07 10:49:10 +0530

    GSAP basics

---
+++ script.js
@@ -0,0 +1,34 @@
+// gsap.to("#box1",{
+//     x:1000,
+//     duration :2,
+//     delay:1
+// })
+
+// gsap.to("#box2",{
+//     x:500,
+//     y:500,
+//     duration :2,
+//     delay:1
+// })
+
+
+// gsap.to("#box1",{
+//     x:1200,
+//     duration :2,
+//     delay:1
+// })
+
+// gsap.from("#box2",{
+//     x:1200,
+//     duration :2,
+//     delay:1
+// })
+
+gsap.to("#box1",{
+    x:1200,
+    duration :2,
+    delay:1,
+    rotate:360,
+    backgroundColor: "blue",
+    borderRadius : "50%",
+})
